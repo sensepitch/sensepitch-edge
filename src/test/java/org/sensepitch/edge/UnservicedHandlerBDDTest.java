cf33ea18f462dcdb7de269febfd7df20216cd047
Jens Wilke <jw_github@headissue.com>
2025-07-23 16:59:25 +0700

    touch up

--- src/test/java/org/sensepitch/edge/UnservicedHandlerBDDTest.java
+++ src/test/java/org/sensepitch/edge/UnservicedHandlerBDDTest.java
@@ -6,6 +6,9 @@ import static io.netty.handler.codec.http.HttpResponseStatus.FOUND;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 import static io.netty.handler.codec.http.HttpMethod.GET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.sensepitch.edge.SanitizeHostHandler.MISSING_HOST;
+import static org.sensepitch.edge.SanitizeHostHandler.UNKNOWN_HOST;
+import static org.sensepitch.edge.UnservicedHandler.NOT_FOUND_URI;
 
 import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
@@ -23,13 +26,17 @@ import java.util.List;
 @ExtendWith(SerenityJUnit5Extension.class)
 class UnservicedHandlerBDDTest {
 
+  // aliases for status codes for better readability
+  static final HttpResponseStatus STATUS_400_BAD_REQUEST = HttpResponseStatus.BAD_REQUEST;
+  static final HttpResponseStatus STATUS_302_FOUND = HttpResponseStatus.FOUND;
+
   @Steps
   UnservicedSteps steps;
 
   @Test
   void allowedHost_passesThrough() {
     steps
-      .given_configuration()
+      .given_a_common_example_configuration()
       .when_request_to("bar.com", "/")
       .then_request_is_passed_to_next_handler();
   }
@@ -37,12 +44,12 @@ class UnservicedHandlerBDDTest {
   @Test
   void missingHost_resultsInBadRequestStatus() {
     steps
-      .given_configuration()
+      .given_a_common_example_configuration()
       .when_request_without_host_to_uri("/")
-      .then_response_status_is(BAD_REQUEST)
+      .then_response_status_is(STATUS_400_BAD_REQUEST)
       .then_location_header_is_absent()
       .when_request_without_host_to_uri("/something")
-      .then_response_status_is(BAD_REQUEST)
+      .then_response_status_is(STATUS_400_BAD_REQUEST)
       .then_location_header_is_absent();
 
   }
@@ -50,52 +57,52 @@ class UnservicedHandlerBDDTest {
   @Test
   void missingHostAfterSanitize_resultsInBadRequestStatus() {
     steps
-      .given_configuration()
-      .when_request_to(SanitizeHostHandler.MISSING_HOST, "/")
-      .then_response_status_is(BAD_REQUEST)
+      .given_a_common_example_configuration()
+      .when_request_to(MISSING_HOST, "/")
+      .then_response_status_is(STATUS_400_BAD_REQUEST)
       .then_location_header_is_absent();
   }
 
   @Test
   void unknownHost_resultsInBadRequestStatus() {
     steps
-      .given_configuration()
-      .when_request_to(SanitizeHostHandler.UNKNOWN_HOST, "/")
-      .then_response_status_is(BAD_REQUEST)
+      .given_a_common_example_configuration()
+      .when_request_to(UNKNOWN_HOST, "/")
+      .then_response_status_is(STATUS_400_BAD_REQUEST)
       .then_location_header_is_absent();
   }
 
   @Test
   void mappedHost_redirectsToMappedTarget() {
     steps
-      .given_configuration()
+      .given_a_common_example_configuration()
       .when_request_to("foo.com", "/")
-      .then_response_status_is(FOUND)
+      .then_response_status_is(STATUS_302_FOUND)
       .then_location_header_is("https://www.foo.com");
   }
 
   @Test
   void otherHost_redirectsToDefaultTarget() {
     steps
-      .given_configuration()
+      .given_a_common_example_configuration()
       .when_request_to("other.com", "/")
-      .then_response_status_is(FOUND)
+      .then_response_status_is(STATUS_302_FOUND)
       .then_location_header_is("https://default.example");
   }
 
   @Test
   void otherHostWithUri_redirectsToDefaultTarget() {
     steps
-      .given_configuration()
+      .given_a_common_example_configuration()
       .when_request_to("other.com", "/anything")
-      .then_response_status_is(FOUND)
-      .then_location_header_is("https://default.example" + UnservicedHandler.NOT_FOUND_URI);
+      .then_response_status_is(STATUS_302_FOUND)
+      .then_location_header_is("https://default.example" + NOT_FOUND_URI);
   }
 
   @Test
   void secondRequestWorks() {
     steps
-      .given_configuration()
+      .given_a_common_example_configuration()
       .when_request_without_host_to_uri("/")
       .then_response_status_is(BAD_REQUEST)
       .then_location_header_is_absent()
@@ -108,14 +115,14 @@ class UnservicedHandlerBDDTest {
 
     private EmbeddedChannel channel;
     private HttpRequest request;
-    private boolean forwarded;
-    private Object forwardedMsg;
-    private HttpResponse lastResponse;
+    private boolean passed;
+    private Object passedMessage;
+    private HttpResponse response;
 
     @Step(
       "When has common configuration: " +
       "passDomains=www.foo.com,bar.com,www.baz.com; defaultTarget=https://default.example")
-    public UnservicedSteps given_configuration() {
+    public UnservicedSteps given_a_common_example_configuration() {
       RedirectConfig cfg = RedirectConfig.builder()
         .passDomains(List.of("www.foo.com", "bar.com", "www.baz.com"))
         .defaultTarget("https://default.example")
@@ -128,12 +135,11 @@ class UnservicedHandlerBDDTest {
     public UnservicedSteps when_request_to(String host, String uri) {
       request = new DefaultHttpRequest(HTTP_1_1, GET, uri);
       request.headers().set(HttpHeaderNames.HOST, host);
-
-      forwarded = channel.writeInbound(request);
-      if (forwarded) {
-        forwardedMsg = channel.readInbound();
+      passed = channel.writeInbound(request);
+      if (passed) {
+        passedMessage = channel.readInbound();
       } else {
-        lastResponse = (HttpResponse) channel.readOutbound();
+        response = (HttpResponse) channel.readOutbound();
         consumeIngressContent();
       }
       return this;
@@ -142,34 +148,34 @@ class UnservicedHandlerBDDTest {
     @Step
     public UnservicedSteps when_request_without_host_to_uri(String uri) {
       request = new DefaultHttpRequest(HTTP_1_1, GET, uri);
-      forwarded = channel.writeInbound(request);
-      lastResponse = (HttpResponse) channel.readOutbound();
+      passed = channel.writeInbound(request);
+      response = (HttpResponse) channel.readOutbound();
       consumeIngressContent();
       return this;
     }
 
     @Step
     public UnservicedSteps then_request_is_passed_to_next_handler() {
-      assertThat(forwarded).isTrue();
-      assertThat(forwardedMsg).isSameAs(request);
+      assertThat(passed).isTrue();
+      assertThat(passedMessage).isSameAs(request);
       return this;
     }
 
     @Step
     public UnservicedSteps then_response_status_is(HttpResponseStatus status) {
-      assertThat(lastResponse.status()).isEqualTo(status);
+      assertThat(response.status()).isEqualTo(status);
       return this;
     }
 
     @Step
     public UnservicedSteps then_location_header_is_absent() {
-      assertThat(lastResponse.headers().get(HttpHeaderNames.LOCATION)).isNull();
+      assertThat(response.headers().get(HttpHeaderNames.LOCATION)).isNull();
       return this;
     }
 
     @Step
     public UnservicedSteps then_location_header_is(String expected) {
-      assertThat(lastResponse.headers().get(HttpHeaderNames.LOCATION)).isEqualTo(expected);
+      assertThat(response.headers().get(HttpHeaderNames.LOCATION)).isEqualTo(expected);
       return this;
     }
 
@@ -177,7 +183,6 @@ class UnservicedHandlerBDDTest {
       HttpContent chunk = new DefaultHttpContent(Unpooled.buffer());
       channel.writeInbound(chunk);
       assertThat(chunk.refCnt()).isZero();
-
       HttpContent last = new DefaultLastHttpContent(Unpooled.buffer());
       channel.writeInbound(last);
       assertThat(last.refCnt()).isZero();
